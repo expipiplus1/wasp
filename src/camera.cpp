a6d37bc5b5b4fc24a6f36124397dccbec0ffbc85
Joe Hermaszewski <t-joherm@microsoft.com>
2011-06-29 10:15:16 +0100

    [+] Added camera class

---
+++ src/camera.cpp
@@ -0,0 +1,82 @@
+/*
+    Copyright 2011 Joe Hermaszewski. All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification, are
+    permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this list of
+    conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice, this list
+    of conditions and the following disclaimer in the documentation and/or other materials
+    provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY Joe Hermaszewski "AS IS" AND ANY EXPRESS OR IMPLIED
+    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Joe Hermaszewski OR
+    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+    ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    The views and conclusions contained in the software and documentation are those of the
+    authors and should not be interpreted as representing official policies, either expressed
+    or implied, of Joe Hermaszewski.
+*/
+
+#include "camera.hpp"
+
+#include <joemath/joemath.hpp>
+
+namespace NWasp
+{
+    CCamera::CCamera    ( )
+    {
+    }
+
+    CCamera::~CCamera   ( )
+    {
+    }
+
+    void    CCamera::SetPosition        ( const float3&   position )
+    {
+        m_position = position;
+    }
+
+    void    CCamera::SetView            ( const float4x4& view )
+    {
+        m_view = view;
+    }
+
+    void    CCamera::SetProjection      ( const float4x4& projection )
+    {
+        m_projection = projection;
+    }
+
+    void    CCamera::Update             ( )
+    {
+        m_viewProjection = m_view * m_projection;
+    }
+
+    const float3&       CCamera::GetPosition         ( ) const
+    {
+        return m_position;
+    }
+
+    const float4x4&     CCamera::GetView             ( ) const
+    {
+        return m_view;
+    }
+    
+    const float4x4&     CCamera::GetProjection       ( ) const
+    {
+        return m_projection;
+    }
+
+    const float4x4&     CCamera::GetViewProjection   ( ) const
+    {
+        return m_viewProjection;
+    }
+};
---
+++ src/camera.hpp
@@ -0,0 +1,62 @@
+/*
+    Copyright 2011 Joe Hermaszewski. All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification, are
+    permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this list of
+    conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice, this list
+    of conditions and the following disclaimer in the documentation and/or other materials
+    provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY Joe Hermaszewski "AS IS" AND ANY EXPRESS OR IMPLIED
+    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Joe Hermaszewski OR
+    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+    ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    The views and conclusions contained in the software and documentation are those of the
+    authors and should not be interpreted as representing official policies, either expressed
+    or implied, of Joe Hermaszewski.
+*/
+
+#pragma once
+
+#include <joemath/joemath.hpp>
+
+using namespace NJoeMath;
+
+namespace NWasp
+{
+    class CCamera
+    {
+    public:
+        CCamera                 ( );
+        virtual ~CCamera        ( );
+
+        void            SetPosition         ( const float3&   position );
+        void            SetView             ( const float4x4& view );
+        void            SetProjection       ( const float4x4& projection );
+        void            Update              ( );
+        
+        const float3&   GetPosition         ( ) const;
+        const float4x4& GetView             ( ) const;
+        const float4x4& GetProjection       ( ) const;
+        const float4x4& GetViewProjection   ( ) const;
+    private:
+        float3      m_position;
+
+        //
+        // Matrices
+        //
+        float4x4    m_view;
+        float4x4    m_projection;
+        float4x4    m_viewProjection;
+    };
+};
