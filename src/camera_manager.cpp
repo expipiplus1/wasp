5cdd6bb18904f0378288f78392568fc602cdea0f
Joe Hermaszewski <t-joherm@microsoft.com>
2011-07-08 14:35:59 +0100

    [+] Created Camera Manager class

---
+++ src/camera_manager.cpp
@@ -0,0 +1,76 @@
+/*
+    Copyright 2011 Joe Hermaszewski. All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification, are
+    permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this list of
+    conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice, this list
+    of conditions and the following disclaimer in the documentation and/or other materials
+    provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY Joe Hermaszewski "AS IS" AND ANY EXPRESS OR IMPLIED
+    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Joe Hermaszewski OR
+    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+    ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    The views and conclusions contained in the software and documentation are those of the
+    authors and should not be interpreted as representing official policies, either expressed
+    or implied, of Joe Hermaszewski.
+*/
+
+#include "camera_manager.hpp"
+
+#include <cassert>
+#include "camera.hpp"
+
+namespace NWasp
+{
+    CCameraManager* CCameraManager::s_instance = nullptr;
+
+    CCameraManager::CCameraManager( )
+    {
+    }
+
+    CCameraManager::~CCameraManager( )
+    {
+    }
+
+    bool        CCameraManager::Create     ( )
+    {
+        assert( s_instance == nullptr );
+        s_instance = new CCameraManager();
+
+        return true;
+    }
+
+    CCameraManager*    CCameraManager::Instance   ( )
+    {
+        assert( s_instance != nullptr );
+        return s_instance;
+    }
+
+    void        CCameraManager::Destroy    ( )
+    {
+        assert( s_instance != nullptr );
+        delete s_instance;
+        s_instance = nullptr;
+    }
+
+    void        CCameraManager::SetCurrentCamera    ( CCamera* current_camera )
+    {
+        m_currentCamera = current_camera;
+    }
+
+    CCamera*    CCameraManager::GetCurrentCamera    ( ) const
+    {
+        return m_currentCamera;
+    }
+};
